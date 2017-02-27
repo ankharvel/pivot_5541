85cecab5e182ff151e4d6ebf62bf7cb3ab9e8221
oAvellaneda <oavellaneda@easysol.net>
2017-02-27 14:08:17 -0500

    HouseKeeping

--- pivot-client/src/main/java/net/sweng/domain/TableCatalogue.java
+++ pivot-client/src/main/java/net/sweng/domain/TableCatalogue.java
@@ -1,9 +1,7 @@
 package net.sweng.domain;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.ResourceBundle;
 import java.util.function.Function;
 
 /**
@@ -12,7 +10,6 @@ import java.util.function.Function;
 public class TableCatalogue extends HashMap<String, List<GenericRow>> {
 
     private Function<String, List<GenericRow>> dataSupplier;
-    private ResourceBundle bundle = ResourceBundle.getBundle("messages");
 
     public TableCatalogue(Function<String, List<GenericRow>> dataSupplier) {
         this.dataSupplier = dataSupplier;
@@ -28,16 +25,5 @@ public class TableCatalogue extends HashMap<String, List<GenericRow>> {
             return super.get(key);
         }
     }
-
-//    public List<ColumnDetail> getColumnDetails(Object key) {
-//        List<GenericRow> rowList = get(key);
-//        List<ColumnDetail> details = new ArrayList<>();
-//        rowList.forEach(r -> details.add(
-//                new ColumnDetail(
-//                        r.get(bundle.getString("header_column")).toString(),
-//                        DataType.valueOf(r.get(bundle.getString("header_type")).toString()))
-//        ));
-//        return details;
-//    }
 
 }
