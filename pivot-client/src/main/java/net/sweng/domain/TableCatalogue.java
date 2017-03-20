0b72ea5b95a728b8d3aafec3ddafc3517142b066
oAvellaneda <oavellaneda@easysol.net>
2017-03-20 12:30:05 -0400

    POM files updated

--- pivot-client/src/main/java/net/sweng/domain/TableCatalogue.java
+++ pivot-client/src/main/java/net/sweng/domain/TableCatalogue.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.function.Function;
 
 /**
+ * This object stores the list of columns (names and types) for every file uploaded to the system
  * Date on 2/23/17.
  */
 public class TableCatalogue extends HashMap<String, List<GenericRow>> {
